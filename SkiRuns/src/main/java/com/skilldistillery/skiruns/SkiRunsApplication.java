85dfbd0c27299b6c618e25d430f415f9ec467b5e
MF <michael.francavilla@gmail.com>
2025-05-02 18:18:14 -0600

    Create Spring Boot MVC project, configure dependencies.

---
+++ SkiRuns/src/main/java/com/skilldistillery/skiruns/SkiRunsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.skiruns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiRunsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkiRunsApplication.class, args);
+	}
+
+}
---
+++ SkiRuns/src/test/java/com/skilldistillery/skiruns/SkiRunsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.skiruns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiRunsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
